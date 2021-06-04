99af453eb323a1ab75de2c99cba46aaf62daa09e
soutaschool <sasakisouta110@icloud.com>
2021-06-05 00:05:07 +0900

    記述方法について

--- main.js
+++ main.js
@@ -2,19 +2,22 @@ const app = Vue.createApp({
     data() {
         return {
             product: "Hello world",
-            image: 'images/red.jpeg',
+            brand: "Souta Sasaki",
+            selectedVariant: 0,
             inStock: false,
             details: ["1 list", "2 list", "3 list"],
             variants: [
                 {
                     id: 2234,
                     color: "red",
-                    image: "images/red.jpeg"
+                    image: "images/red.jpeg",
+                    quantity: 50,
                 },
                 {
                     id: 2235,
                     color: "yellow",
-                    image: "images/yellow.jpeg"
+                    image: "images/yellow.jpeg",
+                    quantity: 0,
                 },
             ],
             cart: 0,
@@ -25,8 +28,20 @@ const app = Vue.createApp({
             // this.cartはdata属性のcartをさす
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        },
+        computed: {
+            title() {
+                return this.brand + '' + this.product
+            },
+            // 最初のselectedVariant(0)を返す
+            image() {
+                return this.variants[this.selectedVariant].image
+            },
+            inStock() {
+                return this.variants[this.selectedVariant].quantity
+            },
         }
     }
 })
