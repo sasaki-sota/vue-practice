481ec8c57bf1f8785ad22ae548ab0a50cc63b6f4
soutaschool <sasakisouta110@icloud.com>
2021-06-04 21:59:00 +0900

    v-bindの利用

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: "Hello world"
+            product: "Hello world",
+            image: './images/test.jpeg'
         }
     }
 })
